c101e1b49658b1cd7c04678627e3421023624de4
ani <ahakobyanani@gmail.com>
2023-02-06 20:02:42 +0400

    updated

--- src/homework_4/AlgorithmicEx.java
+++ src/homework_4/AlgorithmicEx.java
@@ -4,35 +4,22 @@ import java.util.Random;
 
 public class AlgorithmicEx {
     public static void main(String[] args) {
-        System.out.println(primeFactor(1787866));
+        System.out.println(primeFactor(64));
         System.out.println(randomIsEven());
         random();
-        System.out.println(rice(2,2,12));
-        System.out.println(rice(1,2,12));
-        System.out.println(rice(1,2,5));
-        System.out.println(rice(3,5,24));
-        System.out.println(rice(7,2,18));
-        System.out.println(rice(10,2,18));
+        System.out.println(rice(2,3,9));
     }
 
     public static boolean rice(int small, int big, int goal){
-        if(small + big*5 < goal)
+        if(small + big*5 <  goal)
             return false;
         else if(big*5 >= goal){
-            goal -= goal/5 * 5;
+            goal -= goal/5 * 5; //goal = goal - goal/5*5
             if(goal > small)
                 return false;
             return true;
         }
         return true;
-    }
-
-    public static boolean randomIsEven() {
-        Random rd = new Random();
-        int n = rd.nextInt(101);
-        System.out.println("random number is: " + n);
-        System.out.print("is even: ");
-        return n % 2 == 0;
     }
 
     public static int primeFactor(int n) {
@@ -44,12 +31,21 @@ public class AlgorithmicEx {
             if (n % i == 0) {
                 theLargest = i;
                 n /= i;
+                i--;
             }
             i++;
         }
         return theLargest;
     }
 
+
+    public static boolean randomIsEven() {
+        Random rd = new Random();
+        int n = rd.nextInt(101);
+        System.out.println("random number is: " + n);
+        System.out.print("is even: ");
+        return n % 2 == 0;
+    }
     public static void random() {
         Random rd = new Random();
         int smallest = rd.nextInt(101);
